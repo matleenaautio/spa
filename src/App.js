8c7cddafe7210d5a0e2c9996d4e7eabfad8ab796
Matleena Autio <c0auma00@students.oamk.fi>
2021-09-20 20:29:44 +0300

    spa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import NavBar from "./NavBar";
+import Header from "./Header";
+import Home from "./Home";
+import About from "./About";
+import ContactUs from "./ContactUs";
+import NotFound from "./NotFound";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <NavBar />
+    <Header />
+    <div className="container">
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/contactus" component={ContactUs} />
+        <Route component={NotFound} />
+      </Switch>
+
     </div>
+    <Footer />
+    </>
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+        <div>
+            <span className="text-muted">This is footer</span>
+        </div>
+        </footer>
+    )
+}
